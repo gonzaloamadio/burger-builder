162df52bdc1b1eb67befaf5510b6a625d8a34504
gonzaloamadio <gonzaloamadio@gmail.com>
2019-08-03 13:07:22 +1000

    General Layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import classes from './Layout.module.css'
 
 const layout = (props) => (
     <React.Fragment>
-    <div>Toolbar, Sidebar, Backdrop</div>
-    <main>
-        {props.children}
-    </main>
+        <div>Toolbar, Sidebar, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
     </React.Fragment>
 );
 
