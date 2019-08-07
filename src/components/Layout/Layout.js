2fc48ba9cfb97fa5180616b1eafa8beba47b6988
gonzaloamadio <gonzaloamadio@gmail.com>
2019-08-07 21:05:43 +1000

    Add hamburguer menu in toolbar. To close or open Side Menu
     Changes to be committed:
    	modified:   src/components/Layout/Layout.js
    	new file:   src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
    	new file:   src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.module.css
    	modified:   src/components/Navigation/SideDrawer/SideDrawer.js
    	modified:   src/components/Navigation/Toolbar/Toolbar.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <React.Fragment>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </React.Fragment>
-);
+// We use state here to manage/coordinate which components to show and hide.
+// For example, nav items.
+export default class Layout extends Component {
 
-export default layout
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer : false})
+    }
+
+    sideDrawerToggleHandler = () => {
+        this.setState( (prevState) => {
+            return {showSideDrawer : ! prevState.showSideDrawer}
+        })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <Toolbar 
+                    drawerToggleClicked={this.sideDrawerToggleHandler}
+                />
+                <SideDrawer 
+                    closed={this.sideDrawerClosedHandler} 
+                    open={this.state.showSideDrawer} 
+                />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </React.Fragment>
+            )
+    }
+}
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,22 +2,29 @@ import React from 'react'
 import Logo from '../../Logo/Logo'
 import NavigationItems from '../NavigationItems/NavigationsItems'
 import classes from './SideDrawer.module.css'
+import Backdrop from '../../UI/Backdrop/Backdrop'
 
 // Component for Side Menu. Will display in small devices.
 // Animation, and styles in css module.
 // Reuse navigation elements.
 
 const SideDrawer = (props) => {
-
+    let attachedClasses = [classes.SideDrawer, classes.Close]
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open]
+    }
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <React.Fragment>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </React.Fragment>
     )
 }
 
