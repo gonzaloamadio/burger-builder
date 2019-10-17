dd2786b5281df697ef8e53889c48b8ee33080079
Gonzalo Amadio <gonzaloamadio@gmail.com>
2019-10-17 01:46:49 -0300

    FIX: reset price

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -40,7 +40,8 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         ingredients: action.ingredients,
-        error: false
+        error: false,
+        totalPrice: initialState['totalPrice']
       };
     case actionTypes.FETCH_INGREDIENTS_FAILED:
       return {
