8ce6b392a7989cf4dfd96a478c0c94a2f646e232
Gonzalo Amadio <gonzaloamadio@gmail.com>
2019-11-09 11:12:52 -0300

    Show use of call function

--- src/store/sagas/auth.js
+++ src/store/sagas/auth.js
@@ -1,6 +1,6 @@
 import * as actions from '../actions';
 // put will dispatch a new action
-import { put, delay } from 'redux-saga/effects';
+import { put, delay, call } from 'redux-saga/effects';
 import axios from 'axios';
 
 // action: action that was dispatched
@@ -8,9 +8,9 @@ export function* logoutSaga(action) {
   // We need to prepend each step we execute with yield
   // Thah will mean that we have to wait for that step to finish execution.
   // It will not continuew until the step is done
-  yield localStorage.removeItem('token');
-  yield localStorage.removeItem('expirationDate');
-  yield localStorage.removeItem('userId');
+  yield call([localStorage, 'removeItem'], 'token')
+  yield call([localStorage, 'removeItem'], 'expirationDate')
+  yield call([localStorage, 'removeItem'], 'userId')
   yield put(actions.logoutSucceed());
 }
 
